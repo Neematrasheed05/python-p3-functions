a5c7a492f1b23c091a3f599e2b511e798529e538
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-11-21 14:56:53 +0300

    changed default

--- lib/functions.py
+++ lib/functions.py
@@ -3,11 +3,19 @@
 def greet_programmer():
     print("Hello, programmer!")
 
-def greet(name="neemat"):
-    print(f"Hello {name}!")
+#!/usr/bin/env python3
+
+def greet(name="programmer"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print(f"Hello {name}!")
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    greet()  # This will print "Hello, programmer!"
+    greet("Guido")  # This will print "Hello, Guido!"
+    greet_with_default()  # This will print "Hello, programmer!"
+    greet_with_default("Guido")  # This will print "Hello, Guido!"
 
 def add(num1, num2):
     return num1 + num2
